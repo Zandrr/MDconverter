6ce07b186cdc836ccd756803627a38c9ded500e3
Zandrr <alexander.campbell@colorado.edu>
2014-12-07 14:25:02 -0700

    added blockquote to be removed

--- wordcount.php
+++ wordcount.php
@@ -7,7 +7,6 @@ require('Parsedown.php');
     $Parsedown = new Parsedown();
     $str = file_get_contents($argv[1]);
     $str = $Parsedown->text($str);
-    print_r($str);
     $str = preg_replace('/<(code|style|blockquote)(?:(?!<\/\1).)*?<\/\1>/s', '', $str);
     $str = preg_replace("/\```[^)]+\```/", '', $str); //remove all between ```
     print_r(str_word_count(preg_replace('/[^a-z0-9]/i', '_', strip_tags(strtolower($str))), 0));
