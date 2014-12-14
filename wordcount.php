59cd56f761d08d170587eeac652f9770db0ec7b4
Alex Campbell <alexander.campbell@colorado.edu>
2014-12-14 11:27:49 -0700

    Update wordcount.php

--- wordcount.php
+++ wordcount.php
@@ -7,8 +7,8 @@ require('Parsedown.php');
     $str = preg_replace('/<(code|style|blockquote)(?:(?!<\/\1).)*?<\/\1>/s', '', $str);//remove all code, style and blockquotes
     $str = preg_replace("/\```[^)]+\```/", '', $str); //remove all between ```
    	$str = preg_replace('/[^a-z0-9]/i', '_', strip_tags(strtolower($str)), 0);//only keep a-z and 0-9 and then strip tagd
-   	$str = preg_replace('/[.“”’]/', '', $str);//removes '.','"','’' so lab3.pdf is one w#ord
-    $str = preg_replace('/https([\S]+)/', '', $str);
-    $str = preg_replace('/http([\S]+)/', '', $str);
+   	$str = preg_replace('/[.“”’]/', '', $str); //removes '.','"','’' so lab3.pdf is one w#ord
+    $str = preg_replace('/https([\S]+)/', '', $str); //remove words that begin with https (for links)
+    $str = preg_replace('/http([\S]+)/', '', $str);  //remove words that begin with http (for links)
     print_r(str_word_count($str, 0, '0..9'));//words with numbers arent split
 ?>
